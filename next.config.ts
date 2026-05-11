cabc535af2c750ec694572af2090b2389be8e97f
tianreformis <tianreformis@gmail.com>
2026-05-11 15:09:58 +0700

    Midtrans : fix payment Error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   allowedDevOrigins: ['192.168.144.212'],
+   allowedDevOrigins: ['192.168.144.212','earwig-disallow-subway.ngrok-free.dev'],
 };
 
 export default nextConfig;
